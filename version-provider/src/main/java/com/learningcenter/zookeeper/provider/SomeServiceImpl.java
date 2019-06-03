d38f0345d3bc862ce484a4ee13b518ed77b3fedb
Jason.Zhang <285290078@qq.com>
2019-06-03 22:45:26 +0800

    dubbo 多版本控制

---
+++ version-provider/src/main/java/com/learningcenter/zookeeper/provider/SomeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.learningcenter.zookeeper.provider;
+
+import com.learningcenter.api.SomeService;
+
+/**
+ * <pre>
+ * @author Jason
+ *
+ * @version 0.9
+ *
+ * 修改版本: 0.9
+ * 修改日期: 2019-06-03
+ * 修改人 : Jason
+ * 修改说明:
+ * 复审人 ：
+ * </pre>
+ **/
+public class SomeServiceImpl implements SomeService {
+	@Override
+	public String sayHello(String name) {
+		System.out.println("some,"+name);
+		return "some,"+name;
+	}
+}
